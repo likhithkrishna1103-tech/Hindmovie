0a72e481896d1e9cc1c8d2d877d9b683a1648022
likhithkrishna1103-tech <likhithkrishna1103@gmail.com>
2026-04-26 15:43:55 +0530

    yy

--- kaido/plugin.js
+++ kaido/plugin.js
@@ -1,5 +1,5 @@
 (function () {
-    const MAIN_URL = "https://kaido.to";
+    const MAIN_URL = "https://kaido.vercel.app/";
 
     const HEADERS = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0",
